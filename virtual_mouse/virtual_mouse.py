ddd346b0360e45d5f0ad859d1f124616930a7a0b
KenwoodFox <kenwood364@gmail.com>
2023-06-26 10:45:26 -0400

    Simple drawing primitives

--- virtual_mouse/virtual_mouse.py
+++ virtual_mouse/virtual_mouse.py
@@ -11,8 +11,8 @@ from .opengl_tools import *
 class VirtualMouse(object):
     def __init__(self):
         super().__init__()
-        self.backgroundColor = (0, 0, 0, 1)
-        self.window = impl_glfw_init()
+        self.backgroundColor = (0.2, 0.2, 0.2, 1)
+        self.window = impl_glfw_init(window_name="Virtual Mouse")
         gl.glClearColor(*self.backgroundColor)
         imgui.create_context()
         self.impl = GlfwRenderer(self.window)
@@ -25,7 +25,9 @@ class VirtualMouse(object):
             glfw.poll_events()
             self.impl.process_inputs()
             imgui.new_frame()
-            imgui.begin("Custom window", True)
+
+            # Save and Load Maze
+            imgui.begin("Maze Controls", True)
 
             imgui.text("Hello, world!")
 
@@ -37,10 +39,19 @@ class VirtualMouse(object):
 
             _, self.f = imgui.slider_float("float", self.f, 0.25, 1.5)
 
-            imgui.show_test_window()
-
             imgui.end()
 
+            imgui.begin("Actual Maze", True)
+            draw_list = imgui.get_window_draw_list()
+            draw_list.add_triangle(
+                20, 35, 90, 35, 55, 80, imgui.get_color_u32_rgba(1, 1, 0, 1)
+            )
+            draw_list.add_triangle(
+                110, 35, 180, 35, 145, 80, imgui.get_color_u32_rgba(1, 0, 0, 1), 5
+            )
+            imgui.end()
+
+            # Render to screen
             imgui.render()
 
             gl.glClearColor(*self.backgroundColor)
